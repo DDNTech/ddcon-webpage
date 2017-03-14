4e1d23a8475a1ae761cf6cfe763793fd857bd3d1
Chintan Parikh <chintan@pennywhale.com>
2017-03-14 18:13:38 -0400

    Add team info on hover

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -6,4 +6,15 @@ $(window).scroll(function() {
     $(".brand img").removeClass("hidden");
     $("#scroll-to-top").addClass("hidden");
   }
+});
+
+$(document).ready(function() {
+  $(".team-item").mouseenter(function() {
+    $(this).find('.overlay').fadeIn(300).css("display","flex");
+  });
+
+  $(".team-item").mouseleave(function() {
+    $(this).find('.overlay').fadeOut(300)
+  })
+
 })
